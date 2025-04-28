cc6a4c74b7b1decce75dda88183710f5a8cee821
BekaMeskhoradze <beqa.mesxoradze97@gmail.com>
2025-04-28 21:57:03 +0400

    Add homework 34

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Lecturers(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True, null=True)
+
+    class Meta:
+        db_table = 'lecturers'
+
+class Courses(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    lecturer = models.ForeignKey(Lecturers, on_delete= models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = 'courses'
+
+class Students(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=100, unique=True, null=True)
+    grade = models.IntegerField()
+    courses = models.ManyToManyField(Courses)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'students'
--- first_project/settings.py
+++ first_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from password.password import DATABASE_PASSWORD
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'first_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'PP-25_django',
+        'USER':'postgres',
+        'PASSWORD':DATABASE_PASSWORD,
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -104,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tbilisi'
 
 USE_I18N = True
 
